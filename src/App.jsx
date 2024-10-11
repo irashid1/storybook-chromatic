a6063766343a9f79ab6acb3173842d44107f0f6c
Imtiaz Rashid <imtiazrashid@Imtiazs-MacBook-Pro.local>
2024-10-11 15:08:28 -0400

    minor changes to fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
       <p>Hello World!</p>
+      <p>Happy Thanksgiving!</p>
     </>
   )
 }
